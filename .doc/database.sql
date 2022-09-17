99ea1de2a758663c3e5980fc402b635f3df89ced
Cpasklaire <80284454+Cpasklaire@users.noreply.github.com>
2022-09-17 07:35:00 +0200

    maj db

--- .doc/database.sql
+++ .doc/database.sql
@@ -2,32 +2,58 @@ Create database blogphp;
 
 use blogphp;
 
-CREATE TABLE Users (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    pseudo VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    mdp VARCHAR(100) NOT NULL,
-    admin BOOLEAN DEFAULT false,
 
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    archivedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE `posts` (
+  `id` int NOT NULL,
+  `userId` int NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `content` longtext NOT NULL,
+  `chapo` varchar(500) DEFAULT NULL,
+  `postId` int DEFAULT NULL,
+  `valided` tinyint(1) DEFAULT '0',
+  `author` varchar(20) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `archivedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Posts (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    userId INT NOT NULL,
-    title VARCHAR(100),
-    content LONGTEXT NOT NULL,
+INSERT INTO `posts` (`id`, `userId`, `title`, `content`, `chapo`, `postId`, `valided`, `author`, `createdAt`, `updatedAt`, `archivedAt`) VALUES
+(1, 19, 'HTML : le squelette', '<p>Post emensos insuperabilis expeditionis eventus languentibus partium animis, quas periculorum varietas fregerat et laborum, nondum tubarum cessante clangore vel milite locato per stationes hibernas, fortunae saevientis procellae tempestates alias rebus infudere communibus per multa illa et dira facinora Caesaris Galli, qui ex squalore imo miseriarum in aetatis adultae primitiis ad principale culmen insperato saltu provectus ultra terminos potestatis delatae procurrens asperitate nimia cuncta foedabat. propinquitate enim regiae stirpis gentilitateque etiam tum Constantini nominis efferebatur in fastus, si plus valuisset, ausurus hostilia in auctorem suae felicitatis, ut videbatur.\r\n\r\nInter quos Paulus eminebat notarius ortus in Hispania, glabro quidam sub vultu latens, odorandi vias periculorum occultas perquam sagax. is in Brittanniam missus ut militares quosdam perduceret ausos conspirasse Magnentio, cum reniti non possent, iussa licentius supergressus fluminis modo fortunis conplurium sese repentinus infudit et ferebatur per strages multiplices ac ruinas, vinculis membra ingenuorum adfligens et quosdam obterens manicis, crimina scilicet multa consarcinando a veritate longe discreta. unde admissum est facinus impium, quod Constanti tempus nota inusserat sempiterna.\r\n\r\nExcogitatum est super his, ut homines quidam ignoti, vilitate ipsa parum cavendi ad colligendos rumores per Antiochiae latera cuncta destinarentur relaturi quae audirent. hi peragranter et dissimulanter honoratorum circulis adsistendo pervadendoque divites domus egentium habitu quicquid noscere poterant vel audire latenter intromissi per posticas in regiam nuntiabant, id observantes conspiratione concordi, ut fingerent quaedam et cognita duplicarent in peius, laudes vero supprimerent Caesaris, quas invitis conpluribus formido malorum inpendentium exprimebat.</p>', 'Tela cautela quod grassatores trucidantur potuerint circumspecta tela cum petere circumspecta observatum contingit quae circumspecta lacertos exsertare edita bina milites terna quae petere circumspecta trucidantur iniquitati reperiri lacertos potuerint autem planitie adsidue lacertos ob trucidantur contingit coeperint contingit cautela vehunt quod nec in crispare ob observatum cautela circumspecta rem cautela.', NULL, 0, 'Admin', '2022-08-31 01:35:16', '2022-08-31 01:37:30', '2022-08-31 01:35:16'),
+(2, 19, 'CSS : un peu de maquillage', '<p>Isdem diebus Apollinaris Domitiani gener, paulo ante agens palatii Caesaris curam, ad <strong>Mesopotamiam</strong> missus a socero per militares numeros immodice scrutabatur, an quaedam altiora meditantis iam Galli secreta susceperint scripta, qui conpertis Antiochiae gestis per minorem Armeniam lapsus Constantinopolim petit exindeque per protectores retractus artissime tenebatur.</p>\r\n<p>Sed cautela nimia in peiores haeserat plagas, ut narrabimus postea, aemulis consarcinantibus insidias graves apud Constantium, cetera medium principem sed siquid auribus eius huius modi quivis infudisset ignotus,<strong> acerbum</strong> et inplacabilem et in hoc causarum titulo dissimilem sui.</p>\r\n<p>Quibus occurrere bene pertinax miles explicatis ordinibus parans hastisque feriens scuta qui habitus iram pugnantium concitat et dolorem proximos iam gestu terrebat sed eum in certamen alacriter consurgentem revocavere ductores rati intempestivum anceps subire certamen cum haut longe muri distarent, quorum tutela securitas poterat in solido locari cunctorum.</p>\r\n<p>Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.</p>\r\n<p>Ex his quidam aeternitati se commendari posse per statuas aestimantes eas ardenter adfectant quasi plus praemii de figmentis aereis sensu carentibus adepturi, quam ex conscientia honeste recteque factorum, easque auro curant inbracteari, quod<strong> Acilio Glabrioni </strong>delatum est primo, cum consiliis armisque regem superasset Antiochum. quam autem sit pulchrum exigua haec spernentem et minima ad ascensus verae gloriae tendere longos et arduos, ut memorat vates Ascraeus, Censorius Cato monstravit. qui interrogatus quam ob rem inter multos... statuam non haberet malo inquit ambigere bonos quam ob rem id non meruerim, quam quod est gravius cur inpetraverim mussitare.</p>', 'Propinquitate enim regiae stirpis gentilitateque etiam tum Constantini nominis efferebatur in fastus.', NULL, 0, 'Admin', '2022-08-31 01:39:17', '2022-09-17 04:32:13', '2022-08-31 01:39:17'),
+(3, 19, 'Java et Java Script', '<p>Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.</p>\r\n<p>Has autem provincias, quas Orontes ambiens amnis imosque pedes Cassii montis illius celsi praetermeans funditur in Parthenium mare, Gnaeus Pompeius superato Tigrane regnis Armeniorum abstractas dicioni Romanae coniunxit.</p>\r\n<p>Victus universis caro ferina est lactisque abundans copia qua sustentantur, et herbae multiplices et siquae alites capi per aucupium possint, et plerosque mos vidimus frumenti usum et vini penitus ignorantes.</p>\r\n<p>Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi. praediximus enim Montium sub ipso vivendi termino his vocabulis appellatos fabricarum culpasse tribunos ut adminicula futurae molitioni pollicitos.</p>\r\n<p>Ideo urbs venerabilis post superbas efferatarum gentium cervices oppressas latasque leges fundamenta libertatis et retinacula sempiterna velut frugi parens et prudens et dives Caesaribus tamquam liberis suis regenda patrimonii iura permisit.</p>', 'Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat?', NULL, 0, 'Admin', '2022-08-31 01:40:21', '2022-08-31 01:41:14', '2022-08-31 01:40:21'),
+(4, 19, 'Les erreurs et le catch', '<p>Isdem diebus Apollinaris Domitiani gener, paulo ante agens palatii Caesaris curam, ad&nbsp;<strong>Mesopotamiam</strong>&nbsp;missus a socero per militares numeros immodice scrutabatur, an quaedam altiora meditantis iam Galli secreta susceperint scripta, qui conpertis Antiochiae gestis per minorem Armeniam lapsus Constantinopolim petit exindeque per protectores retractus artissime tenebatur.</p>\r\n<p>Sed cautela nimia in peiores haeserat plagas, ut narrabimus postea, aemulis consarcinantibus insidias graves apud Constantium, cetera medium principem sed siquid auribus eius huius modi quivis infudisset ignotus,<strong>&nbsp;acerbum</strong>&nbsp;et inplacabilem et in hoc causarum titulo dissimilem sui.</p>\r\n<p>Quibus occurrere bene pertinax miles explicatis ordinibus parans hastisque feriens scuta qui habitus iram pugnantium concitat et dolorem proximos iam gestu terrebat sed eum in certamen alacriter consurgentem revocavere ductores rati intempestivum anceps subire certamen cum haut longe muri distarent, quorum tutela securitas poterat in solido locari cunctorum.</p>\r\n<p>Ipsam vero urbem Byzantiorum fuisse refertissimam atque ornatissimam signis quis ignorat? Quae illi, exhausti sumptibus bellisque maximis, cum omnis Mithridaticos impetus totumque Pontum armatum affervescentem in Asiam atque erumpentem, ore repulsum et cervicibus interclusum suis sustinerent, tum, inquam, Byzantii et postea signa illa et reliqua urbis ornanemta sanctissime custodita tenuerunt.</p>\r\n<p>Ex his quidam aeternitati se commendari posse per statuas aestimantes eas ardenter adfectant quasi plus praemii de figmentis aereis sensu carentibus adepturi, quam ex conscientia honeste recteque factorum, easque auro curant inbracteari, quod<strong>&nbsp;Acilio Glabrioni&nbsp;</strong>delatum est primo, cum consiliis armisque regem superasset Antiochum. quam autem sit pulchrum exigua haec spernentem et minima ad ascensus verae gloriae tendere longos et arduos, ut memorat vates Ascraeus, Censorius Cato monstravit. qui interrogatus quam ob rem inter multos... statuam non haberet malo inquit ambigere bonos quam ob rem id non meruerim, quam quod est gravius cur inpetraverim mussitare.</p>', 'Quanta autem vis amicitiae sit, ex hoc intellegi maxime potest.', NULL, 0, 'Admin', '2022-09-17 03:44:58', '2022-09-17 04:32:20', '2022-09-17 03:44:58'),
+(5, 44, NULL, 'Post emensos insuperabilis expeditionis eventus languentibus partium animis, quas periculorum varietas fregerat et laborum, nondum tubarum cessante clangore vel milite locato per stationes hibernas, fortunae saevientis procellae tempestates alias rebus infudere communibus per multa illa et dira facinora Caesaris Galli', NULL, 1, 0, 'Shun', '2022-09-17 03:54:58', '2022-09-17 03:54:58', '2022-09-17 03:54:58'),
+(6, 44, NULL, 'Ex his quidam aeternitati se commendari posse per statuas aestimantes eas ardenter adfectant quasi plus praemii de figmentis aereis sensu carentibus adepturi, quam ex conscientia honeste recteque factorum, easque auro curant inbracteari, quod Acilio Glabrioni delatum est primo, cum consiliis armisque regem superasset Antiochum. quam autem sit pulchrum exigua haec spernentem et minima ad ascensus verae gloriae tendere longos et arduos, ut memorat vates Ascraeus, Censorius Cato monstravit. qui interrogatus quam ob rem inter multos... statuam non haberet malo inquit ambigere bonos quam ob rem id non meruerim, quam quod est gravius cur inpetraverim mussitare.', NULL, 2, 1, 'Shun', '2022-09-17 03:55:16', '2022-09-17 03:58:12', '2022-09-17 03:55:16'),
+(7, 44, NULL, 'Quibus occurrere bene pertinax miles explicatis ordinibus parans hastisque feriens scuta qui habitus iram pugnantium concitat et dolorem proximos iam gestu terrebat sed eum in certamen alacriter consurgentem revocavere ductores rati intempestivum anceps subire certamen cum haut longe muri distarent, quorum tutela securitas poterat in solido locari cunctorum.', NULL, 2, 0, 'Shun', '2022-09-17 03:55:28', '2022-09-17 03:55:28', '2022-09-17 03:55:28'),
+(8, 44, NULL, 'Acilio Glabrioni delatum est primo, cum consiliis armisque regem superasset Antiochum.', NULL, 4, 0, 'Shun', '2022-09-17 04:00:17', '2022-09-17 04:00:17', '2022-09-17 04:00:17');
 
-    postId INTEGER,
-    valided BOOLEAN DEFAULT false,
-    author VARCHAR(100),
 
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    archivedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-    FOREIGN KEY (postId) REFERENCES Posts(id),
-    FOREIGN KEY (userId) REFERENCES Users(id)
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mdp` varchar(100) NOT NULL,
+  `admin` tinyint(1) DEFAULT '0',
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `archivedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO `users` (`id`, `pseudo`, `email`, `mdp`, `admin`, `createdAt`, `updatedAt`, `archivedAt`) VALUES
+(19, 'Admin', 'admin@mail.com', '$2y$10$GVuopNUQQmppe.kd63FEuuf76U7lbSfppHYmGI8OvFWvVI/r.NRwK', 1, '2022-08-31 01:17:04', '2022-08-31 01:17:33', '2022-08-31 01:17:04'),
+(42, 'Anonyme', 'not@existe.fr', 'unsuperlongmotdepassequipassepasparlehachcommeçac\'estencoreplusdureàtrouver', 0, '2022-08-31 03:04:59', '2022-09-15 09:54:30', '2022-08-31 03:04:59'),
+(44, 'Shun', 'shun@dandromede.com', '$2y$10$KHCrdBBfWOWT0Wlulc8Xv.iVJsIy4RbNCS.cCXvhGVtHio5AvwDlW', 0, '2022-09-15 09:57:28', '2022-09-15 09:57:28', '2022-09-15 09:57:28');
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `pseudo` (`pseudo`);
+
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `posts_ibfk_1` (`postId`),
+  ADD KEY `posts_ibfk_2` (`userId`),
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
--- src/lib/DatabaseConnection.php
+++ src/lib/DatabaseConnection.php
@@ -15,7 +15,7 @@ class DatabaseConnection
 /*              $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
             $dotenv->load();  */
 
-            $this->database = new \PDO('mysql:host=localhost;dbname=baseblog;charset=utf8', 'root', 'root');
+            $this->database = new \PDO('mysql:host=localhost;dbname=blogphp;charset=utf8', 'root', 'root');
         }
         return $this->database;
     }
